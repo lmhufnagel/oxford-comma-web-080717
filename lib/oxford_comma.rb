07a996a0e1a81e6590bb9adb6f774dccfd336014
Lisa Hufnagel <lmhufnagel@gmail.com>
2017-08-01 22:55:39 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  if  array.length == 2
+    array.join(" and ")
+  elsif array.length == 1
+    array.join
+  else
+    array[-1] = "and #{array[-1]}"
+    array.join(", ")
+  end
 end
